1549269ddaf70f753371ffa3447aa61d137960f5
xlcrr <info@openlittermap.com>
2020-02-18 18:32:08 +0000

    update javascript

--- src/components/VueCsvImport.vue
+++ src/components/VueCsvImport.vue
@@ -79,158 +79,171 @@
 </template>
 
 <script>
-import { drop, every, forEach, get, isArray, map, set } from 'lodash';
-import axios from 'axios';
-import Papa from 'papaparse';
-import mimeTypes from "mime-types";
-
-export default {
-    props: {
-        value: Array,
-        url: {
-            type: String
-        },
-        mapFields: {
-            required: true
-        },
-        callback: {
-            type: Function,
-            default: () => ({})
-        },
-        catch: {
-            type: Function,
-            default: () => ({})
-        },
-        finally: {
-            type: Function,
-            default: () => ({})
-        },
-        parseConfig: {
-            type: Object,
-            default() {
-                return {};
+    import { drop, every, forEach, get, isArray, map, set } from 'lodash';
+    import axios from 'axios';
+    import Papa from 'papaparse';
+    import mimeTypes from "mime-types";
+    export default {
+        props: {
+            value: Array,
+            url: {
+                type: String
+            },
+            mapFields: {
+                required: true
+            },
+            callback: {
+                type: Function,
+                default: () => ({})
+            },
+            catch: {
+                type: Function,
+                default: () => ({})
+            },
+            finally: {
+                type: Function,
+                default: () => ({})
+            },
+            parseConfig: {
+                type: Object,
+                default() {
+                    return {};
+                }
+            },
+            headers: {
+                default: null
+            },
+            loadBtnText: {
+                type: String,
+                default: "Next"
+            },
+            submitBtnText: {
+                type: String,
+                default: "Submit"
+            },
+            tableClass: {
+                type: String,
+                default: "table"
+            },
+            checkboxClass: {
+                type: String,
+                default: "form-check-input"
+            },
+            buttonClass: {
+                type: String,
+                default: "btn btn-primary"
+            },
+            inputClass: {
+                type: String,
+                default: "form-control-file"
+            },
+            validation: {
+                type: Boolean,
+                default: true,
+            },
+            fileMimeTypes: {
+                type: Array,
+                default: () => {
+                    return ["text/csv", "text/x-csv", "application/vnd.ms-excel", "text/plain"];
+                }
             }
         },
-        headers: {
-            default: null
-        },
-        loadBtnText: {
-            type: String,
-            default: "Next"
-        },
-        submitBtnText: {
-            type: String,
-            default: "Submit"
-        },
-        tableClass: {
-            type: String,
-            default: "table"
-        },
-        checkboxClass: {
-            type: String,
-            default: "form-check-input"
-        },
-        buttonClass: {
-            type: String,
-            default: "btn btn-primary"
-        },
-        inputClass: {
-            type: String,
-            default: "file-input"
-        },
-        validation: {
-            type: Boolean,
-            default: true,
-        },
-        fileMimeTypes: {
-            type: Array,
-            default: () => {
-                return ["text/csv", "text/x-csv", "application/vnd.ms-excel", "text/plain"];
-            }
-        }
-    },
-
-    data: () => ({
-        form: {
+        data: () => ({
+            form: {
+                csv: null,
+            },
+            fieldsToMap: [],
+            map: {},
+            hasHeaders: true,
             csv: null,
+            sample: null,
+            isValidFileMimeType: false,
+            fileSelected: false
+        }),
+        created() {
+            this.hasHeaders = this.headers;
+            if (isArray(this.mapFields)) {
+                this.fieldsToMap = map(this.mapFields, (item) => {
+                    return {
+                        key: item,
+                        label: item
+                    };
+                });
+            } else {
+                this.fieldsToMap = map(this.mapFields, (label, key) => {
+                    return {
+                        key: key,
+                        label: label
+                    };
+                });
+            }
         },
-        fieldsToMap: [],
-        map: {},
-        hasHeaders: true,
-        csv: null,
-        sample: null,
-        isValidFileMimeType: false,
-        fileSelected: false,
-        filename: ''
-    }),
-
-    created() {
-        this.hasHeaders = this.headers;
-
-        if (isArray(this.mapFields)) {
-            this.fieldsToMap = map(this.mapFields, (item) => {
-                return {
-                    key: item,
-                    label: item
-                };
-            });
-        } else {
-            this.fieldsToMap = map(this.mapFields, (label, key) => {
-                return {
-                    key: key,
-                    label: label
-                };
-            });
-        }
-    },
-    
-    buildMappedCsv() {
-
-        const _this = this;
-
-        let csv = this.hasHeaders ? drop(this.csv) : this.csv;
-
-        return map(csv, (row) => {
-            let newRow = {};
-
-            forEach(_this.map, (column, field) => {
-                set(newRow, field, get(row, column));
-            });
-
-            return newRow;
-        });
-    },
-    
-    validFileMimeType() {
-        let file = this.$refs.csv.files[0];
-        const mimeType = file.type === "" ? mimeTypes.lookup(file.name) : file.type;
-
-            this.readFile((output) => {
-                _this.sample = get(Papa.parse(output, { preview: 2, skipEmptyLines: true }), "data");
-                _this.csv = get(Papa.parse(output, { skipEmptyLines: true }), "data");
-            });
-        },
-
-        readFile(callback) {
-          let file = this.$refs.csv.files[0];
-
-          this.readFile((output) => {
-              _this.sample = _.get(Papa.parse(output, { preview: 2, skipEmptyLines: true }), "data");
-              _this.csv = _.get(Papa.parse(output, { skipEmptyLines: true }), "data");
-          });
-        },
-        
-        readFile(callback) {
-            let file = this.$refs.csv.files[0];
-
-            if (file) {
-                let reader = new FileReader();
-                reader.readAsText(file, "UTF-8");
-                reader.onload = function (evt) {
-                    callback(evt.target.result);
-                };
-                reader.onerror = function () {
-                };
+        methods: {
+            submit() {
+                const _this = this;
+                this.form.csv = this.buildMappedCsv();
+                this.$emit('input', this.form.csv);
+                if (this.url) {
+                    axios.post(this.url, this.form).then(response => {
+                        _this.callback(response);
+                    }).catch(response => {
+                        _this.catch(response);
+                    }).finally(response => {
+                        _this.finally(response);
+                    });
+                } else {
+                    _this.callback(this.form.csv);
+                }
+            },
+            buildMappedCsv() {
+                const _this = this;
+                let csv = this.hasHeaders ? drop(this.csv) : this.csv;
+                return map(csv, (row) => {
+                    let newRow = {};
+                    forEach(_this.map, (column, field) => {
+                        set(newRow, field, get(row, column));
+                    });
+                    return newRow;
+                });
+            },
+            validFileMimeType() {
+                let file = this.$refs.csv.files[0];
+                const mimeType = file.type === "" ? mimeTypes.lookup(file.name) : file.type;
+                if (file) {
+                    this.fileSelected = true;
+                    this.isValidFileMimeType = this.validation ? this.validateMimeType(mimeType) : true;
+                } else {
+                    this.isValidFileMimeType = !this.validation;
+                    this.fileSelected = false;
+                }
+            },
+            validateMimeType(type) {
+                return this.fileMimeTypes.indexOf(type) > -1;
+            },
+            load() {
+                const _this = this;
+                this.readFile((output) => {
+                    _this.sample = get(Papa.parse(output, { preview: 2, skipEmptyLines: true }), "data");
+                    _this.csv = get(Papa.parse(output, { skipEmptyLines: true }), "data");
+                });
+            },
+            readFile(callback) {
+                let file = this.$refs.csv.files[0];
+                if (file) {
+                    let reader = new FileReader();
+                    reader.readAsText(file, "UTF-8");
+                    reader.onload = function (evt) {
+                        callback(evt.target.result);
+                    };
+                    reader.onerror = function () {
+                    };
+                }
+            },
+            toggleHasHeaders() {
+                this.hasHeaders = !this.hasHeaders;
+            },
+            makeId(id) {
+                return `${id}${this._uid}`;
             }
         },
         watch: {
@@ -243,16 +256,12 @@ export default {
                         }) : every(this.mapFields, function (item, key) {
                             return newVal.hasOwnProperty(key);
                         });
-                    if (hasAllKeys) {
-                        this.submit();
+                        if (hasAllKeys) {
+                            this.submit();
+                        }
                     }
                 }
             }
-        }
-    },
-    computed: {
-        firstRow() {
-            return _.get(this, "sample.0");
         },
         computed: {
             firstRow() {
@@ -265,9 +274,5 @@ export default {
                 return !this.isValidFileMimeType;
             }
         },
-        disabledNextButton() {
-            return !this.isValidFileMimeType;
-        }
-    },
-};
+    };
 </script>
